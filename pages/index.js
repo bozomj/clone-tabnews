1413d4a18bcec84ee5b6a4a9f22952c00460b41d
bozomj <38231286+bozomj@users.noreply.github.com>
2024-11-27 23:49:24 +0000

    inicio

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>"Aceite as coisas às quais o destino une você, e ame as pessoas com quem o destino o reuniu, mas faça isso de todo o coração"</h1>
+}
+
+export default Home;
