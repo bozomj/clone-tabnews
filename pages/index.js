381d108ee3ceeb55c5420fc1229006ce2d3197ce
bozomj <38231286+bozomj@users.noreply.github.com>
2024-11-29 00:58:41 +0000

    segundo deploy

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home(){
-    return <h1>"Aceite as coisas às quais o destino une você, e ame as pessoas com quem o destino o reuniu, mas faça isso de todo o coração"</h1>
+    return <di>
+    <h1>"Aceite as coisas às quais o destino une você, e ame as pessoas com quem o destino o reuniu, mas faça isso de todo o coração"</h1>
+    <h2> segundo deploy </h2>
+</di>;
+
 }
 
 export default Home;
