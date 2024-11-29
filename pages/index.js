14b40c73232cbb59d7abecfba6c3eda2b12db8b6
bozomj <38231286+bozomj@users.noreply.github.com>
2024-11-29 01:14:21 +0000

    segunda frase removida

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 function Home(){
     return <di>
-    <h1>"Aceite as coisas às quais o destino une você, e ame as pessoas com quem o destino o reuniu, mas faça isso de todo o coração"</h1>
-    <h2> segundo deploy </h2>
-</di>;
+                <h1>"Aceite as coisas às quais o destino une você, e ame as pessoas com quem o destino o reuniu, mas faça isso de todo o coração"</h1>
+                
+            </di>;
 
 }
 
