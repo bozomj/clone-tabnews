66a7268f1d3b257c59ddac40d4af69d4b07bddbb
bozomj <38231286+bozomj@users.noreply.github.com>
2024-12-04 02:01:03 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,12 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("select 1 + 1 as SUM;");
+  console.log(result.rows);
+
   response.status(200).json({
     aluno: "alunos do curso.dev são pessoas acima da média",
   });
 }
- 
+
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,4 @@
 test("GET TO /API/V1/STATUS SHOULD RETURN 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  console.log(response.headers);
   expect(response.status).toBe(200);
 });
