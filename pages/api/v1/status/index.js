895d4c2272108c3a3c49684ad4aed4e47e32d8b8
bozomj <38231286+bozomj@users.noreply.github.com>
2024-12-06 02:21:32 +0000

    finaliza finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,27 @@
-// import database from "../../../../infra/database.js";
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("select 1 + 1 as SUM;");
-  console.log(result.rows);
+  const updatedAT = new Date().toISOString();
+
+  const postgresVersion = await database.query({
+    text: "show server_version",
+  });
+  const postgresMaxConnections = await database.query("show max_connections;");
+
+  const dbName = process.env.POSTGRES_DB;
+  const usedConnections = await database.query({
+    // text: "select count(*) as used_connections from pg_stat_activity where datname = $1 and state = $2",
+    text: "select count(*) as used_connections from pg_stat_activity where datname = $1;",
+    values: [dbName],
+  });
+
+  console.log(`Banco de dados selecionado é: ${dbName}`);
 
   response.status(200).json({
-    aluno: "alunos do curso.dev são pessoas acima da média",
+    updated_at: updatedAT,
+    postgres_version: postgresVersion.rows[0].server_version,
+    max_connections: parseInt(postgresMaxConnections.rows[0].max_connections),
+    used_connections: parseInt(usedConnections.rows[0].used_connections),
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET TO /API/V1/STATUS SHOULD RETURN 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.postgres_version).toContain("16.0");
+  expect(responseBody.max_connections).toEqual(expect.any(Number));
+  expect(responseBody.used_connections).toEqual(1);
 });
