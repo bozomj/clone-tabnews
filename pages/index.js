969f995b10a857524b67ce23c04707e39c6d2424
bozomj <38231286+bozomj@users.noreply.github.com>
2025-03-04 00:26:59 +0000

    refactor: restor index page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home() {
     <di>
       <h1>
         &quot; Aceite as coisas às quais o destino une você, e ame as pessoas
-        com quem o destino o reuniu, mas faça isso de todo o coração &quot;
+        com quem o destino o reuniu, mas faça isso de todo o coração blz &quot;
       </h1>
     </di>
   );
