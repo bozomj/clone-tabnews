1b3a4c2e464270950ab6562f424c0a7b369a7bd7
bozomj <38231286+bozomj@users.noreply.github.com>
2026-03-21 03:55:51 +0000

    fix: 'change email from in email.send'

--- models/activation.js
+++ models/activation.js
@@ -108,7 +108,7 @@ async function create(userId) {
 
 async function sendEmailToUser(user, activationToken) {
   await email.send({
-    from: "FinTab <contato@fintab.com.br>",
+    from: "FinTab <contato@bzmj.com.br>",
     to: user.email,
     subject: "Ative seu cadastro no FinTab!",
     text: `${user.username}, clique no link abaixo para ativar seu cadastro no FinTab:
