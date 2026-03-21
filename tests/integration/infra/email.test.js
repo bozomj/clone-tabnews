29a400bfe25dc09642194785178549d506471342
bozomj <38231286+bozomj@users.noreply.github.com>
2026-03-21 14:33:28 +0000

    test: remove bug in test

--- tests/integration/infra/email.test.js
+++ tests/integration/infra/email.test.js
@@ -10,21 +10,21 @@ describe("infra/email.js", () => {
     await orchestrator.deleteAllEmails();
 
     await email.send({
-      from: "FinTab <contato@fintab.com.br>",
+      from: "FinTab <contato@bzmj.com.br>",
       to: "contato@curso.dev",
       subject: "Teste de assunto",
       text: "Test de corpo.",
     });
 
     await email.send({
-      from: "FinTab <contato@fintab.com.br>",
+      from: "FinTab <contato@bzmj.com.br>",
       to: "contato@curso.dev",
       subject: "penultimo email enviado",
       text: "corpo do penultimo email",
     });
 
     await email.send({
-      from: "FinTab <contato@fintab.com.br>",
+      from: "FinTab <contato@bzmj.com.br>",
       to: "contato@curso.dev",
       subject: "ultimo email enviado",
       text: "corpo do ultimo email",
@@ -32,7 +32,7 @@ describe("infra/email.js", () => {
 
     const lastEmail = await orchestrator.getLastEmail();
 
-    expect(lastEmail.sender).toBe("<contato@fintab.com.br>");
+    expect(lastEmail.sender).toBe("<contato@bzmj.com.br>");
     expect(lastEmail.recipients[0]).toBe("<contato@curso.dev>");
     expect(lastEmail.subject).toBe("ultimo email enviado");
   });
